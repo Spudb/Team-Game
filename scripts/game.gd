e6bfd00e8908c178652b6365dba47a6d8ec1865a
NileKnight0 <ammartube777@gmail.com>
2026-09-17 23:53:45 +0300

    added simple tent, bg sky and basic temp player and noe starting in first game

---
+++ scripts/game.gd
@@ -0,0 +1,13 @@
+extends Node2D
+
+
+
+func _ready() -> void:
+	pass # Replace with function body.
+
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_tent_1_body_entered(body: Node2D) -> void:
+	pass # Replace with function body.
