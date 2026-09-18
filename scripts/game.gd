4becbce38fb45a5cb613efd01b44f3e6cf261132
NileKnight0 <ammartube777@gmail.com>
2026-09-18 21:32:46 +0300

    adde unfo screen bedide tent

--- scripts/game.gd
+++ scripts/game.gd
@@ -10,8 +10,11 @@ func _process(delta: float) -> void:
 		if tent_shooter:
 			get_tree().change_scene_to_file("res://scenes/shooter.tscn")
 	
-	pass
-
+	
+	
+	$map/shooter_tent/info.position.x = remap($player.position.x, 320, 800, 90, -130)
+	
+	#print($player.position)
 
 var tent_shooter = 0
 func _on_tent_shooter_body_entered(body: Node2D) -> void:
@@ -21,8 +24,22 @@ func _on_tent_shooter_body_entered(body: Node2D) -> void:
 func _on_tent_shooter_body_exited(body: Node2D) -> void:
 	if body.name == "player":
 		tent_shooter = 0
-
-
-
-
 #
+#func _on_tent1_left_body_entered(body: Node2D) -> void:
+	#if body.name == "player":
+		#$map/shooter_tent/info_left.visible = 1
+#func _on_tent1_left_body_exited(body: Node2D) -> void:
+	#if body.name == "player":
+		#$map/shooter_tent/info_left.visible = 0
+#func _on_tent1_right_body_entered(body: Node2D) -> void:
+	#if body.name == "player":
+		#$map/shooter_tent/info_right.visible = 1
+#func _on_tent1__right_body_exited(body: Node2D) -> void:
+	#if body.name == "player":
+		#$map/shooter_tent/info_right.visible = 0
+func _on_tent1_info_area_body_entered(body: Node2D) -> void:
+	if body.name == "player":
+		$map/shooter_tent/info.visible = 1
+func _on_tent1_info_area_body_exited(body: Node2D) -> void:
+	if body.name == "player":
+		$map/shooter_tent/info.visible = 0
