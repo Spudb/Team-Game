0890836ad76e9057e7d12d75c76240b2089c530f
Spudb <mostafazyad285@gmail.com>
2026-09-18 21:17:08 +0300

    Map
    Added interaction with the chest, and opening animation.
    Added a map that the player can enable when opening the chest.

--- TreasureHunt/TreasureHunt Scripts/map_chest.gd
+++ TreasureHunt/TreasureHunt Scripts/map_chest.gd
@@ -1,13 +1,35 @@
 extends StaticBody2D
 
+var is_opened = false
+var is_in = false
+
 @onready var label: Label = $Label
+@onready var sub_viewport_container: SubViewportContainer = $"../CanvasLayer/SubViewportContainer"
+@onready var player: CharacterBody2D = $"../Player"
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
 func _ready() -> void:
 	pass
 
+func _physics_process(delta: float) -> void:
+	chest_interaction()
+
 func _on_area_2d_body_entered(body: Node2D) -> void:
-	var is_opened = false
-	label.show()
+	is_in = true
+	if is_opened == false:
+		label.show()
+	else:
+		pass
+	
 
 func _on_area_2d_body_exited(body: Node2D) -> void:
 	label.hide()
+	is_in = false
+
+func chest_interaction():
+	if Input.is_action_just_pressed("Interact"):
+		if is_in == true:
+			if is_opened == false:
+				sub_viewport_container.show()
+				animated_sprite_2d.play("opened")
+				is_opened = true
--- TreasureHunt/TreasureHunt Scripts/player.gd
+++ TreasureHunt/TreasureHunt Scripts/player.gd
@@ -1,15 +1,23 @@
 extends CharacterBody2D
 
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+@onready var camera_2d: Camera2D = $"../CanvasLayer/SubViewportContainer/SubViewport/Camera2D"
 
 @export var SPEED = 300.0
 @export var JUMP_VELOCITY = -400.0
 
 
+func _ready() -> void:
+	var sub_viewport: SubViewport = $"../CanvasLayer/SubViewportContainer/SubViewport"
+	if sub_viewport:
+		sub_viewport.world_2d = get_viewport().world_2d
+
 func _physics_process(delta):
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+	
+	camera_2d.position = global_position
 
 	# Get the input direction and handle the movement/deceleration.
 	var direction := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
